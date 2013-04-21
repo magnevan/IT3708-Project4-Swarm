dbe5dd7f4294a81cebd53b87d124e91519f6f3ec
Edvard Kristoffer Karlsen <edvardkk@ulfenville.(none)>
2013-04-21 22:39:49 +0200

    Added some minimal retrival behavior

--- swarm-epuck/controllers/botty/retrival.py
+++ swarm-epuck/controllers/botty/retrival.py
@@ -2,6 +2,10 @@ import random
 from behavior import Behavior
 
 
+RETRIVAL_THRESHOLD =   200
+PUSH_THRESHOLD     =  2000
+
+
 
 class RetrivalBehavior(Behavior):
     """
@@ -13,75 +17,21 @@ class RetrivalBehavior(Behavior):
         self._t = random.randint(0, 20)
 
     def act(self, inputs):
-        self._t += 1
-        should_supress = self._t % 20 < 5
-        return (-1, -1,), should_supress
+        highest_intensity = max(inputs)
 
+        should_retrive = highest_intensity >= RETRIVAL_THRESHOLD
+        if not should_retrive:
+            return (0, 0,), False
 
+        print inputs
 
-class Retrival:
-    PUSH_THRESHOLD = 500
+        #max_off_input = max(inputs[1:])
+        should_push = (inputs[0] >= PUSH_THRESHOLD and
+                       inputs[1] >= PUSH_THRESHOLD)
 
-    left_wheel_speed = 0
-    right_wheel_speed = 0
-
-    LED = [False]*8 #LED lights state
-
-    '''
-    Internal functions
-    '''
-
-    def update_speed(IR_number):
-        cases = {0:(700,0),
-                 1:(350,0),
-                 2:(550,-300),
-                 3:(500,0)}
-        transf = [0,1,2,3,3,2,1,0]
-
-        motors = cases[tranf[IR_number]]
-        lp, rp = motors if IR_number < 4 else reversed(motors)
-
-        left_wheel_speed += lp
-        right_wheel_speed += rp
-
-    # The movement for converging to the box
-    def converge_to_box(IR_sensors, IR_threshold):
-        left_wheel_speed = right_wheel_speed = 0
-
-        LED = [False]*8
-        for i, val in enumerate(IR_sensors):
-            if val < IR_threshold:
-                LED[i] = True
-                update_speed(i)
-
-
-    # The behavior when pushing the box
-    def push_box(IR_sensors, IR_threshold):
-        left_wheel_speed = right_wheel_speed = 0
-
-        # Blink LED
-        LED = [not l for l in LED]
-
-        if IR_sensors[0] < IR_threshold and IR_sensors[7] < IR_threshold:
-            left_wheel_speed = 1000
-            right_wheel_speed = 1000
+        if should_push:
+            output = 1, 1
         else:
-            # Update speeds for all triggering sensors
-            (update_speed(i) for i,v in enumerate(IR_sensors) if v < IR_threshold)
+            output = .5, 0
 
-
-    # Selects the behavior push or converge
-    def select_behavior(IR_sensors):
-        is_pushing = any(ir < PUSH_THRESHOLD for ir in IR_sensors)
-
-    '''
-    External functions
-    '''
-
-    # Converge, push, and stagnation recovery
-    def swarm_retrival(IR_sensors, IR_threshold):
-        select_behavior(IR_sensors)
-        if select_behavior(IR_sensors):
-            push_box(IR_sensors, IR_threshold)
-        else:
-            converge_to_box(IR_sensors, IR_threshold)
+        return output, True
