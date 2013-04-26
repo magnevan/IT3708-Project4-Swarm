11390bb469c94e94b3b91547452c000c2eb74f11
Edvard Kristoffer Karlsen <edvardkk@ulfenville.(none)>
2013-04-26 03:34:27 +0200

    Added submission preparation script.

---
+++ prepare_submission.bash
@@ -0,0 +1,9 @@
+PREFIX=it3708_epuck_karlsen_vikjord_amundsen
+CODE_TEMP_DIR=${PREFIX}_code
+
+cd report && make && cd ..
+cp report/report.pdf ${PREFIX}_report.pdf
+cp -R swarm-epuck ${CODE_TEMP_DIR}
+find ${CODE_TEMP_DIR} -name '*git*' | xargs rm -fr
+zip -r ${PREFIX}_code.zip ${CODE_TEMP_DIR}
+rm -fr ${CODE_TEMP_DIR}
