d02c8266fab4e7ab3a08334763c4f3dd7f7d36b2
Edvard Kristoffer Karlsen <edvardkk@ulfenville.(none)>
2013-04-26 03:37:32 +0200

    Fixed Jonas' name, removed waste.

--- prepare_submission.bash
+++ prepare_submission.bash
@@ -5,5 +5,6 @@ cd report && make && cd ..
 cp report/report.pdf ${PREFIX}_report.pdf
 cp -R swarm-epuck ${CODE_TEMP_DIR}
 find ${CODE_TEMP_DIR} -name '*git*' | xargs rm -fr
+find ${CODE_TEMP_DIR} -name '*wbproj*' | xargs rm -fr
 zip -r ${PREFIX}_code.zip ${CODE_TEMP_DIR}
 rm -fr ${CODE_TEMP_DIR}
